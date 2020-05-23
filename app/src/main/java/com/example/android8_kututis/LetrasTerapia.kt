467fab50e7cbefecd357352680512b2cf9d6e63c
Fernando Ponce <exminaruto@gmail.com>
2020-05-23 15:42:34 -0500

    Inicio sesion Google

---
+++ app/src/main/java/com/example/android8_kututis/LetrasTerapia.kt
@@ -0,0 +1,28 @@
+package com.example.android8_kututis
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import kotlinx.android.synthetic.main.activity_letras_terapia.*
+
+
+class LetrasTerapia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letras_terapia)
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            val personName = acct.displayName
+            val personEmail = acct.email
+            val personId = acct.id
+            val personPhoto: Uri? = acct.photoUrl
+
+            tvNombre.text = personName
+            tvEmail.text = personEmail
+            tvId.text = personId
+        }
+    }
+}
