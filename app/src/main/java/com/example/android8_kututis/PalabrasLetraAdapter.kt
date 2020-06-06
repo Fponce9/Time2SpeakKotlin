c0914ea875eff969cfa6b2a352e9d1bc35fbedc6
PieroAranda <51999788+PieroAranda@users.noreply.github.com>
2020-06-05 23:34:26 -0500

    Imagen dinamica

--- app/src/main/java/com/example/android8_kututis/PalabrasLetraAdapter.kt
+++ app/src/main/java/com/example/android8_kututis/PalabrasLetraAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.android8_kututis
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android8_kututis.Network.Palabra
-import kotlinx.android.synthetic.main.letras_terapia_row.view.*
 import kotlinx.android.synthetic.main.palabras_letra_row.view.*
 
 class PalabrasLetraAdapter(val Palabras:List<Palabra>):RecyclerView.Adapter<PalabrasViewHolder>() {
@@ -23,6 +24,12 @@ class PalabrasLetraAdapter(val Palabras:List<Palabra>):RecyclerView.Adapter<Pala
         val palabrasTitle=Palabras.get(position)
         holder.view.row_palabra_id.text=palabrasTitle.palabra
         holder.palabra=palabrasTitle.palabra
+
+        val parseo:ByteArray = palabrasTitle.imagen.toByteArray()
+
+        val btm:Bitmap = BitmapFactory.decodeByteArray(parseo,0, parseo.size)
+
+        holder.view.ImagenPalabra.setImageBitmap(btm)
     }
 
 }
