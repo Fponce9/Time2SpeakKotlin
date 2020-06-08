3cfc0ec1c847e983a227e2dd77da00e427e3f870
Luis <luisger_mr@hotmail.com>
2020-06-08 01:12:16 -0500

    activity mascota y medalla

---
+++ app/src/main/java/com/example/android8_kututis/Medallas.kt
@@ -0,0 +1,11 @@
+package com.example.android8_kututis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Medallas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medallas)
+    }
+}
